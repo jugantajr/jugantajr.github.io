48d4e8d62f1e4d9c70100accc64668394b45c730
Juganta Rajkhowa <108810583+jugantajr@users.noreply.github.com>
2025-01-29 23:14:58 +0530

    change script location

--- myscripts.js
+++ myscripts.js
@@ -1,9 +1,4 @@
-function toggleDarkMode() {
-    document.body.classList.toggle('dark-mode');
-    localStorage.setItem('darkMode', document.body.classList.contains('dark-mode'));}
-    if (localStorage.getItem('darkMode') === 'true') {
-        document.body.classList.add('dark-mode');
-    }
+
 
     function toggleMenu() {
         const navMenu = document.querySelector('.nav-menu');
